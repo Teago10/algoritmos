00ec8b7dcdf74320a19bb28a64e27de8ff466b49
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-01 22:04:16 -0300

    Declaração de Váriavel Decimal

---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,24 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	static void Main()
+	{
+		int a;
+		// Inicialização da Variável
+		a = 22;
+		Console.WriteLine(a);
+		// Reatribuição de valor na Váriavel
+		a = 56;
+		Console.WriteLine(a);
+		// Outras formas de declarar uma váriavel
+		
+		//Declaração e inicializaçã da Váriavel
+		int b = 925;
+		Console.WriteLine(b);
+		
+		int y = 10, x = 20;
+		int z = y + x;
+		Console.WriteLine(z);
+	}
+}
---
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -0,0 +1,10 @@
+using System;
+
+class DeclaracaoVariaveisDecimal
+{
+	static void Main(string[] args)
+	{
+		decimal myMoneyCorrect = 300.0m;
+		Console.WriteLine(myMoneyCorrect);
+	}
+}
