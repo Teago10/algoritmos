8a7fdb1cb63479688e06bf4debea1eff62da7a61
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-01 22:10:41 -0300

    Declaração Variavel Decimal

--- 02_Variaveis/DeclaracaoVariaveisDecimal.cs
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -4,6 +4,7 @@ class DeclaracaoVariaveisDecimal
 {
 	static void Main(string[] args)
 	{
+		//decimal myMoneyError = 300.0;
 		decimal myMoneyCorrect = 300.0m;
 		Console.WriteLine(myMoneyCorrect);
 	}
