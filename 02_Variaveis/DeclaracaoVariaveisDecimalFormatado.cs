1b7d3bc79e64ee878ed44fa8c96c02f524078f68
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-04 20:51:32 -0300

    Atualização da Declaração de Variavel decimal Formatado

--- 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
@@ -1,12 +1,28 @@
 using System;
-class DeclaracaoVariavelDecimalFormatado;
+class DeclaracaoVariavelDecimalFormatado
 {
 	static void Main(string[] args)
 	{
 		decimal x = 0.999m;
 		decimal y = 9999999999999999999999999999m;
-		Console.WriteLine("Minha quantia = {0;C}",x)
-		Console.WriteLine("Sua quantia = {0;C}", y);
 		
+		/* 
+			Utilizando Placeholder para demonstrar o 
+			conteúdo de uma variavel no texto
+			(0:C)
+			0 = posição de marcação
+			C = Formatação de Moeda(Currency)
+		*/
+		Console.WriteLine("Minha quantia = {0:C}",x);
+		Console.WriteLine("Sua quantia = {0:C}", y);
+		Console.WriteLine("Valor de x = {0:F3} e Valor de y = {1:C}", x, y);
+		
+		/*
+		Interpolação de String
+		
+		Console.WriteLine($"Minha quantia = {x:C}");
+		Console.WriteLine($"Sua quantia = {y:C}");
+		Console.WriteLine($"Valor de x = {x:F3} e Valor de y = {y:C}");
+		*/
 	}
 }
