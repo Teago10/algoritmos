8277e5cd1babf8431d1ad753c761d174fbc5d75d
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-08 20:39:13 -0300

    Declaração de vetores, Loops com for e foreach

--- 04_Vetores/Program.cs
+++ 04_Vetores/Program.cs
@@ -6,7 +6,41 @@ namespace _04_Vetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Declaração de variavel
+            // Sintaxe: tipo nomeVariavel = valor
+            string nome = "Fulano de Tal";
+
+            //Reatribuição de valor em uma variavel
+            nome = "Beltrano";
+            Console.WriteLine(nome);
+
+            //Declaração de vetores
+            //Sintaxe: tipo[] nomeVariavel = {valor1, valor2, valor3}
+            string[] nomes = { "Fulano de Tal", "Beltrano", "Sicrano", "João", "Maria" };
+            Console.WriteLine(nomes[0]);
+            Console.WriteLine(nomes[1]);
+            Console.WriteLine(nomes[2]);
+            
+
+            //Loop FOR = para
+            //for(indice; controle; incremento)
+            // .Length - 
+            for(int i = 0;i < nomes.Length; i++)
+            {
+                Console.WriteLine("{0}° Nome: {1}", i+1, nomes[i]);  //{0} substitui i+1, {1} substitui nomes[i]
+            }
+            //Impressão dos 100 primeiros numeros pares
+            for (int i = 0; i <= 100; i+= 2)
+            {
+                Console.WriteLine("Número: {0}", i);
+            }
+
+            //Loop foreach
+            // Sintaxe: foreach(variavel in vetor)
+            foreach(string varNome in nomes)
+            {
+                Console.WriteLine("Nome: {0}", varNome);
+            }
         }
     }
 }
