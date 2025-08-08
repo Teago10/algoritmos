2d5037e49cb20e78e2af2c1b761c8ca232db5a72
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-07 21:33:56 -0300

    Programa de classificação de idade

--- 03_DesvioCondicional/Par_ou_ImparC#.cs
+++ 03_DesvioCondicional/Par_ou_ImparC#.cs
@@ -1,1 +1,20 @@
 using System;
+
+class ParOuImpar
+{
+	static void Main()
+	{
+		int numero, resultado;
+		Console.WriteLine("Digite um numero: ");
+		numero = int.Parse(Console.ReadLine() );
+		
+		resultado = numero % 2;
+		if (resultado == 0)
+		{
+			Console.WriteLine("O número é Par");
+		}
+		else{
+			Console.WriteLine("O número é Impar");
+		}
+	}
+}
