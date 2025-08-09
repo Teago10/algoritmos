7b71d260e261d15f9ab5a9d39341e85ea51253d9
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-08 22:06:00 -0300

    Tabuada do usuario

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int numero;
+            int resultado;
+
+            Console.WriteLine("Digite um numero: ");
+            numero = int.Parse(Console.ReadLine() );
+            
+
+            while (contador <= 10) 
+            {
+                resultado = numero * contador;
+                Console.WriteLine("{0} x {1} = {2}", numero,contador, resultado);
+                contador++;
+            }
+
+        }
+    }
+}
