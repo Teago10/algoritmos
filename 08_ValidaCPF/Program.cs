04e20081a8bf257b1884237c85495e786f980d01
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-08-22 20:24:50 -0300

    Criação do metodo que calcula o digito verificador

--- 08_ValidaCPF/Program.cs
+++ 08_ValidaCPF/Program.cs
@@ -2,91 +2,79 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 
-internal class Program
+namespace _08_ValidaCPF
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        string cpf, apenasNumeros;
-
-        Console.WriteLine("Informe seu CPF: ");
-        cpf = Console.ReadLine();
-
-        // Remove caracteres não numéricos
-        apenasNumeros = Regex.Replace(cpf, @"\D", "");
-
-        char[] vetorchar = apenasNumeros.ToCharArray();
-
-        // Verifica se tem exatamente 11 dígitos
-        if (vetorchar.Length != 11)
+        static void Main(string[] args)
         {
-            Console.WriteLine("CPF inválido");
-            return;
+
+            Console.Write("Digite o CPF: ");
+            string cpf = Console.ReadLine();
+
+            // 1 - Eliminar caractres não numéricos -------------
+            //cpf.replace("'.", "");
+            //cpf.Replace("'-", "");
+            cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+            // 2 - Validar se tem 11 digitos ----------
+            if (cpf.Length != 11)
+            {
+                Console.WriteLine("CPF deve conter 11 digitos");
+                return;
+            }
+
+            // 3- Validas CPFs com todos os números iguais ------------
+            /* if( cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
+                 cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" ||
+                 cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"                 
+             )*/
+            if (cpf.Distinct().Count() == 1)
+            {
+                Console.WriteLine("CPF inválido! Números repetidos não são permitidos");
+                return;
+            }
+
+            //4 - Cálculo do 1º Digito verificador 
+            int digX = CalculaDV(cpf, 9, 10);
+
+            //5 - Cálculo do 2º Digito Verificador 
+            int digY = CalculaDV(cpf, 10, 11);
+
+            //6 - Comparar os dígitos 
+            if (
+                int.Parse(cpf[9].ToString()) == digX &&
+                int.Parse(cpf[10].ToString()) == digY
+               )
+            {
+                Console.WriteLine("CPF VÁLIDO!");
+            }
+            else
+            {
+                Console.WriteLine("CPF INVÁLIDO!");
+            }
         }
 
-        /* Distinct() - Remove valores duplicados 
-           Count() - Conta quantos valores únicos existem 
-           Se existir apenas 1 valor único, todos são iguais */
-        if (vetorchar.Distinct().Count() == 1)
+        public static int CalculaDV(string cpf, int qtdeNumeros, int peso)
         {
-            Console.WriteLine("CPF inválido");
-            return;
+            int soma = 0;
+            char[] cpfVetor = cpf.ToCharArray();
+
+            for (int i = 0; i < qtdeNumeros; i++)
+            {
+                soma += int.Parse(cpfVetor[i].ToString()) * (peso - i);
+            }
+            int resto = soma % 11;
+
+            int digito = 0;
+            if (resto >= 2)
+            {
+                digito = 11 - resto;
+            }
+            return digito;
         }
 
-        // Pega os 9 primeiros dígitos do CPF para calcular o 1º dígito
-        string vetorcpf = apenasNumeros.Substring(0, 9);
-        int soma = 0;
-
-        // Calcula a soma do produto dos 9 primeiros dígitos pelos multiplicadores
-        for (int i = 0; i < 9; i++)
-        {
-            soma += int.Parse(vetorcpf[i].ToString()) * (10 - i);
-        }
-
-        int resto = soma % 11;
-        int digX;
-
-        // Calcula o 1º dígito verificador usando if/else
-        if (resto < 2)
-        {
-            digX = 0;
-        }
-        else
-        {
-            digX = 11 - resto;
-        }
-
-        soma = 0; // Zera a soma para calcular o 2º dígito
-        int digY = 0;
-
-        // Calcula a soma do produto dos 9 dígitos + 1º dígito verificador pelos multiplicadores
-        for (int i = 0; i < 9; i++)
-        {
-            soma += int.Parse(vetorcpf[i].ToString()) * (11 - i);
-        }
-        soma += digX * 2; // Inclui o 1º dígito verificador com peso 2
-
-        // Calcula o segundo dígito verificador usando if/else
-        resto = soma % 11;
-        if (resto < 2)
-        {
-            digY = 0;
-        }
-        else
-        {
-            digY = 11 - resto;
-        }
-
-        // Comparar os dígitos
-        if (int.Parse(apenasNumeros[9].ToString()) == digX &&
-            int.Parse(apenasNumeros[10].ToString()) == digY)
-        {
-            Console.WriteLine("CPF válido!");
-        }
-        else
-        {
-            Console.WriteLine("CPF inválido!");
-            return;
-        }
-       
     }
 }
+
+
